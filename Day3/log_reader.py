c17a44c54d29d05b0057499c0ebaf4d5a533e0d7
Heet Sheth <97078945+Heet-Sheth@users.noreply.github.com>
2025-12-15 02:23:38 +0530

    List down all the new, solved and unsolved errors.
    Final verdict on current run vs the baseline one.

--- Day3/log_reader.py
+++ Day3/log_reader.py
@@ -17,39 +17,45 @@ def compare_logs(new_run_logfile,baseline_logfile):
     log_new = log_reader(new_run_logfile)
     log_old = log_reader(baseline_logfile)
 
-    new_errors=0
-    solved_errors=0
+    solved_errors={}
+    new_errors={}
+    persistant={}
 
-    print("New Errors...")
+    total_new_error=0
+    total_solved_error=0
+    total_persistant_error=0
 
     for key in log_new:
-        current_count = log_new[key]
+        new_count = log_new[key]
         old_count = log_old.get(key,0)
 
-        if current_count>old_count:
-            print(key,':',current_count-old_count)
-            new_errors+=(current_count-old_count)
-
-    print("Solved Errors...")
+        if new_count>old_count:
+            new_errors[key]=new_count-old_count
+            total_new_error+=(new_count-old_count)
+        elif new_count==old_count:
+            persistant[key]=old_count
+            total_persistant_error+=old_count
 
     for key in log_old:
-        old_count = log_old[key]
-        current_count = log_new.get(key,0)
+        new_count = log_new.get(key,0)
+        old_count = log_old.get(key,0)
 
-        if current_count==0:
-            print(key,': all')
-            solved_errors+=old_count
-        elif current_count<old_count:
-            print(key,':',old_count-current_count)
-            solved_errors+=(current_count-old_count)
+        if old_count>new_count:
+            solved_errors[key]=old_count-new_count
+            total_solved_error+=(old_count-new_count)
 
-    print("Final Verdict")
-    if new_errors>solved_errors:
-        print('FAIL')
-    elif solved_errors>new_errors:
-        print('PASS')
+    print("Solved Errors:",solved_errors)
+    print("New Errors:",new_errors)
+    print("Not Changed:",persistant)
+
+    print("FINAL VERDICT")
+
+    if total_new_error>total_solved_error:
+        print("FAIL")
+    elif total_new_error<total_solved_error:
+        print("PASS")
     else:
-        print('NO CHANGE')
+        print("NO CHANGE")
 
 
 if __name__ == "__main__":
